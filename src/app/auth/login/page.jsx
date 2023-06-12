4b56960af0192062db3542660f996abd9f911ce9
uodev <uygaroztcyln@gmail.com>
2023-06-12 22:24:48 +0300

    created login and register page

--- src/app/auth/login/page.jsx
+++ src/app/auth/login/page.jsx
@@ -7,7 +7,7 @@ function Login() {
         <h1 className="text-white font-extrabold text-5xl mb-6 text-center h-14">
           Giris Yap
         </h1>
-        <div className="bg-[#273746] w-5/12 flex flex-col justify-center items-center rounded-lg shadow-lg shadow-[#17202A]">
+        <div className="bg-secondary-button w-5/12 flex flex-col justify-center items-center rounded-lg shadow-lg shadow-[#17202A]">
           <form className="w-full gap-5 flex flex-col h-fit px-10 py-5">
             <div className="flex flex-col gap-3">
               <label htmlFor="email" className="text-xl">
@@ -18,7 +18,7 @@ function Login() {
                 name="email"
                 id="email"
                 placeholder="Email adresinizi girniz!"
-                className="w-full pl-8 py-4 rounded-full border border-[#353C4A] bg-[#181E29] text-white"
+                className="w-full pl-8 py-4 rounded-full border border-[#353C4A] bg-primary-button text-white"
               />
             </div>
             <div className="flex flex-col gap-3">
@@ -30,20 +30,20 @@ function Login() {
                 name="password"
                 id="password"
                 placeholder="Sifrenizi giriniz!"
-                className="w-full pl-8 py-4 rounded-full border border-[#353C4A] bg-[#181E29] text-white"
+                className="w-full pl-8 py-4 rounded-full border border-[#353C4A] bg-primary-button text-white"
               />
             </div>
             <input
               type="submit"
               value="Giris Yap"
-              className="cursor-pointer hover:bg-[#0B101B]/70 transition-all p-4 bg-[#181E29] rounded-full  border border-[#353C4A] text-white font-semibold mt-5"
+              className="cursor-pointer hover:bg-hover-primary-button transition-all p-4 bg-primary-button rounded-full  border border-[#353C4A] text-white font-semibold mt-5"
             />
           </form>
           <hr className="h-[1px] bg-white w-9/12 mb-5" />
           <div className="mb-5">
-            <p className="text-gray-500">
+            <p className="text-accent/50">
               Hesabiniz yok mu?{" "}
-              <span className="text-white underline cursor-pointer">
+              <span className="text-text underline cursor-pointer">
                 <Link href="/auth/register">Kayit Olun</Link>
               </span>
             </p>
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,8 +10,19 @@ module.exports = {
       colors: {
         backgroundColor: "#0B101B",
         textSecondary: "#C9CED6",
+        text: "#e5eaeb",
+        background: "#070809",
+        "primary-button": "RGBA(7, 8, 9, 0.7)",
+        "hover-primary-button": "RGBA(9, 8, 9, 1)",
+        "secondary-button": "#000000",
+        accent: "#cad6d8",
       },
     },
   },
   plugins: [],
 };
+// --text: #e5eaeb;
+// --background: #070809;
+// --primary-button: #bccacd;
+// --secondary-button: #000000;
+// --accent: #cad6d8;
